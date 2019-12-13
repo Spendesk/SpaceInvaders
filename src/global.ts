3992d6fbcd307fc6d63f47759a1c68a643a8ec27
lili17793 <Elisa-Lucchini_student2018@wilder.school>
2019-12-13 12:45:00 +0100

    add a text that explain the rule at the beginning of the game

--- src/global.ts
+++ src/global.ts
@@ -5,3 +5,7 @@ export const BUG_HEIGHT = 60; // added 2 pixels for padding
 export const FONT_FAMILY = "Monaco, Courier, monospace";
 export const VELOCITY = 200;
 export const VELOCITY_STEP = 50;
+export const INITIAL_DELAY_BUGS = 3000;
+export const INITIAL_DELAY_RECEIPTS = 2500;
+export const DELAY_STEP_BUGS = 300;
+export const DELAY_STEP_RECEIPTS = 270;
